d75291a922143019e781fca7a0cb398a9c3b8c92
Jeet Thakkar <jthak002@ucr.edu>
2017-11-07 11:57:10 -0800

    First Commit

---
+++ cs122a-quadcopter/main.c
@@ -0,0 +1,18 @@
+/*
+ * cs122a-quadcopter.c
+ *
+ * Created: 07-11-2017 11:51:29
+ * Author : Jeet
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
